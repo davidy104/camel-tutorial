636727e3dc3cb4b9599584e3b9ca6f3bd163a18b
David <david.yuan@propellerhead.co.nz>
2014-06-23 12:02:31 +1200

    test

--- camel-tutorial/camel-error/src/main/java/co/nz/camel/tutorial/error/config/ApplicationConfiguration.java
+++ camel-tutorial/camel-error/src/main/java/co/nz/camel/tutorial/error/config/ApplicationConfiguration.java
@@ -1,10 +1,55 @@
 package co.nz.camel.tutorial.error.config;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.apache.camel.CamelContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import co.nz.camel.tutorial.config.CamelSpringContextConfig;
+import co.nz.camel.tutorial.error.dlc.DlcRouteBuilder;
+import co.nz.camel.tutorial.error.dotry.DoTryRouteBuilder;
+import co.nz.camel.tutorial.error.exception.ExceptionRouteBuilder;
+import co.nz.camel.tutorial.error.logging.LoggingRouteBuilder;
+import co.nz.camel.tutorial.error.oncompletion.OnCompletionMultipleRouteBuilder;
+import co.nz.camel.tutorial.error.oncompletion.OnCompletionRouteBuilder;
+import co.nz.camel.tutorial.error.retry.RetryRouteBuilder;
+import co.nz.camel.tutorial.error.retryconditional.RetryConditionalRouteBuilder;
+import co.nz.camel.tutorial.error.retrycustom.RetryCustomRouteBuilder;
+import co.nz.camel.tutorial.error.synchronizations.DynamicOnCompletionRouteBuilder;
 
 @Configuration
 @ComponentScan(basePackages = "co.nz.camel.tutorial.error")
+@Import(value = { CamelSpringContextConfig.class })
 public class ApplicationConfiguration {
+	@Resource
+	private DlcRouteBuilder dlcRouteBuilder;
 
+	@Resource
+	private RetryCustomRouteBuilder retryCustomRouteBuilder;
+
+	@Resource
+	private CamelContext camelContext;
+
+	@PostConstruct
+	public void initializeRoutes() throws Exception {
+		camelContext.addRoutes(dlcRouteBuilder);
+
+		camelContext.addRoutes(new LoggingRouteBuilder());
+
+		camelContext.addRoutes(new RetryRouteBuilder());
+		camelContext.addRoutes(new RetryConditionalRouteBuilder());
+		camelContext.addRoutes(retryCustomRouteBuilder);
+
+		camelContext.addRoutes(new ExceptionRouteBuilder());
+		camelContext.addRoutes(new DoTryRouteBuilder());
+
+		camelContext.addRoutes(new OnCompletionRouteBuilder());
+		camelContext.addRoutes(new OnCompletionMultipleRouteBuilder());
+
+		camelContext.addRoutes(new DynamicOnCompletionRouteBuilder());
+
+	}
 }
--- camel-tutorial/camel-transforming/src/main/java/co/nz/camel/tutorial/transforming/config/ApplicationConfiguration.java
+++ camel-tutorial/camel-transforming/src/main/java/co/nz/camel/tutorial/transforming/config/ApplicationConfiguration.java
@@ -1,10 +1,86 @@
 package co.nz.camel.tutorial.transforming.config;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.apache.camel.CamelContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import co.nz.camel.tutorial.config.CamelSpringContextConfig;
+import co.nz.camel.tutorial.transforming.csv.CsvRouteBuilder;
+import co.nz.camel.tutorial.transforming.enrich.EnrichRouteBuilder;
+import co.nz.camel.tutorial.transforming.enrich.EnrichWithAggregatorRouteBuilder;
+import co.nz.camel.tutorial.transforming.enrich.EnrichXsltRouteBuilder;
+import co.nz.camel.tutorial.transforming.jaxb.JaxbRouteBuilder;
+import co.nz.camel.tutorial.transforming.json.JsonJacksonRouteBuilder;
+import co.nz.camel.tutorial.transforming.json.JsonRouteBuilder;
+import co.nz.camel.tutorial.transforming.normalizer.NormalizerRouteBuilder;
+import co.nz.camel.tutorial.transforming.simple.SimpleRouteBuilder;
+import co.nz.camel.tutorial.transforming.xmljson.XmlJsonRouteBuilder;
+import co.nz.camel.tutorial.transforming.xquery.XqueryParamRouteBuilder;
+import co.nz.camel.tutorial.transforming.xquery.XqueryRouteBuilder;
+import co.nz.camel.tutorial.transforming.xslt.XsltParamRouteBuilder;
+import co.nz.camel.tutorial.transforming.xslt.XsltRouteBuilder;
 
 @Configuration
 @ComponentScan(basePackages = "co.nz.camel.tutorial.transforming")
+@Import(value = { CamelSpringContextConfig.class })
 public class ApplicationConfiguration {
+	@Resource
+	private CamelContext camelContext;
 
+	@Resource
+	private NormalizerRouteBuilder NormalizerRouteBuilder;
+
+	@Resource
+	private EnrichRouteBuilder enrichRouteBuilder;
+
+	@Resource
+	private EnrichWithAggregatorRouteBuilder enrichWithAggregatorRouteBuilder;
+
+	@PostConstruct
+	public void initializeRoutes() throws Exception {
+		camelContext.addRoutes(NormalizerRouteBuilder);
+		camelContext.addRoutes(new SimpleRouteBuilder());
+		camelContext.addRoutes(new JaxbRouteBuilder());
+
+		initXQueryTransformingRoutes();
+		initXsltTransformingRoutes();
+		initJsonTransforming();
+		initXmlJsonTransformingRoutes();
+		initCsvTransforming();
+		initEnrichTransforming();
+
+	}
+
+	private void initXQueryTransformingRoutes() throws Exception {
+		camelContext.addRoutes(new XqueryParamRouteBuilder());
+		camelContext.addRoutes(new XqueryRouteBuilder());
+	}
+
+	private void initXsltTransformingRoutes() throws Exception {
+		camelContext.addRoutes(new XsltParamRouteBuilder());
+		camelContext.addRoutes(new XsltRouteBuilder());
+	}
+
+	private void initJsonTransforming() throws Exception {
+		camelContext.addRoutes(new JsonJacksonRouteBuilder());
+		camelContext.addRoutes(new JsonRouteBuilder());
+	}
+
+	private void initXmlJsonTransformingRoutes() throws Exception {
+		camelContext.addRoutes(new XmlJsonRouteBuilder());
+	}
+
+	private void initCsvTransforming() throws Exception {
+		camelContext.addRoutes(new CsvRouteBuilder());
+	}
+
+	private void initEnrichTransforming() throws Exception {
+		camelContext.addRoutes(new EnrichXsltRouteBuilder());
+		camelContext.addRoutes(enrichWithAggregatorRouteBuilder);
+		camelContext.addRoutes(enrichRouteBuilder);
+	}
 }
